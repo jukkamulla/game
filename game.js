19e856c2e1e3d268304d94f08404e70e452bb00e
jukkamulla <jukkamulla@gmail.com>
2015-01-07 19:19:32 +0100

    create the framework of the game

---
+++ game.js
@@ -0,0 +1,11 @@
+var play = function () {
+    console.log("The game is strated! ");
+    var usersChoice = document.getElementById("userschoice").value;
+    var messages = document.getElementById("messages");
+
+    var result = "result";
+    messages.innerHTML =  result + "</br>" + messages.innerHTML;
+
+};
+
+
